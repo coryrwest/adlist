bce68b0bd8841506b3b36cef5edc529126eca2ab
Cory Westropp <cory.r.west@gmail.com>
2017-08-12 07:48:37 -0700

    List compilation and clean up

--- download-lists.sh
+++ download-lists.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-wget -T 10 -O - http://winhelp2002.mvps.org/hosts.txt > lists/winhelp2002.txt
+wget -T 10 -O - http://winhelp2002.mvps.org/hosts.txt | grep -v '#' | sed "s/0.0.0.0 //" >| lists/winhelp2002.txt
 
-wget -T 10 -O - 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml' > lists/yoyo.txt
+wget -T 10 -O - 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml' | grep -v '#' >| lists/yoyo.txt
 
-wget -T 10 -O - 'http://malwaredomains.lehigh.edu/files/justdomains' > lists/lehigh-malware.txt
+wget -T 10 -O - 'http://malwaredomains.lehigh.edu/files/justdomains' | grep -v '#' >| lists/lehigh-malware.txt
 
-wget -T 10 -O - 'http://www.malwaredomainlist.com/hostslist/hosts.txt' > lists/malwaredomainlist.txt
+wget -T 10 -O - 'http://www.malwaredomainlist.com/hostslist/hosts.txt' | sed "s/127.0.0.1  //" >| lists/malwaredomainlist.txt
 
-wget -T 10 -O - 'https://isc.sans.edu/feeds/suspiciousdomains_High.txt' > lists/isc.sans.edu.txt
+wget -T 10 -O - 'https://isc.sans.edu/feeds/suspiciousdomains_High.txt' | grep -v '#' >| lists/isc.sans.edu.txt
 
 #wget -T 10 -O - 'https://isc.sans.edu/feeds/suspiciousdomains_Medium.txt
 
-wget -T 10 -O - 'https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist' > lists/zeustracker.txt
+wget -T 10 -O - 'https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist' | grep -v '#' >| lists/zeustracker.txt
 
 # Timed out
 #wget -T 10 -O - 'http://mirror2.malwaredomains.com/files/immortal_domains.txt' > malwaredomains.txt
--- merge-lists.sh
+++ merge-lists.sh
@@ -2,4 +2,6 @@
 
 set -o noclobber
 
-cat lists/* | sort | uniq >| master-ad-list.txt
+# clean up the list
+cat lists/* | dos2unix | grep -v '^$' | sort | sed 's/^[[[:alpha:][:digit:].-]]//g' | uniq | awk '{ print "address=/" $0 "/0.0.0.0" }' >| master-ad-list.txt
+
