b81418330c6a46688ad875435775d9957687c661
Cory Westropp <cory.r.west@gmail.com>
2017-09-20 17:31:57 -0700

    Allow google

--- allow.sh
+++ allow.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-grep -v 'googleadservices' master-ad-list.txt > temp
+grep -v 'googleadservices' master-ad-list.txt > temp1
+grep -v 'analytics.google' temp1 > temp
 
 rm master-ad-list.txt
 
